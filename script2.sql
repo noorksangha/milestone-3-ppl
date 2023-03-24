58aad43d5b691e1c8e6ea43679c16c6a4649b30c
Brendan Doyle <104942450+Brendan-Doy1e@users.noreply.github.com>
2023-03-24 14:16:23 -0700

    Update script2.sql

--- script2.sql
+++ script2.sql
@@ -1,1 +1,53 @@
+-- Script 2, queries.
+
+
+
+-- 1: Number of hours each employee worked per week?
+
+
+-- 2:Number of labor hours last week?
+
+
+-- 3: List of which employees worked breaker shifts in the last month?
+
+
+-- 4: How many breaker shifts are scheduled this week?
+
+
+-- 5: How many Slot Attendants are scheduled today?
+
+
+-- 6: Any Slot Attendants who have not been assigned to Section NORTH in the last month
+
+
+-- 7: Number of active Written Warning (WW) employee A has?
+
+
+-- 8: List of active WW sorted by date and employee
+
+
+-- 9: A list of an employee's discipline/performance actions
+
+
+-- 10: Number of sick days an employee has available
+
+
+-- 11: Number of vacation days an employee has available
+
+
+-- 12: Statistics on Employees: Number of Female/Male employees, Average age of employees? Number of employees over 50? Under 30
+
+
+-- 13: List of employees who has the mandatory certification expiring in the next 6 weeks
+
+
+-- 14: List of employees who need updated in-house training
+
+
+-- 15: List of employees who have expired training
+
+
+-- 16: How many uniforms remain un-allocated?
+
+
 
